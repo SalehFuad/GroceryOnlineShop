a5ae02e7e1b204f70559ba63dc37c5f12445397c
SalehFuad <saleh.fuadr@gmail.com>
2023-10-17 11:43:35 +0300

    ---

--- products/views.py
+++ products/views.py
@@ -14,6 +14,7 @@ def about(request):
 def contact(request):
     return render(request, 'contact.html')
 
+
 #fruit list
 def list_fruits(request):
     fruits = Product.objects.filter(category='Fruits')
